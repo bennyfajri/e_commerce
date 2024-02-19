6c6ed98016c97544accd158c0acd479cdcd64579
Muhammad Beni Fajri <m.b.fajrii@gmail.com>
2024-02-19 08:57:01 +0700

    add : firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:e_commerce/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:e_commerce/utils/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main()  {
+import 'firebase_options.dart';
+
+Future<void> main() async  {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+      // .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+
   runApp(const App());
 }
 
