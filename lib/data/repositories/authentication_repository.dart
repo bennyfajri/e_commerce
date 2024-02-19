c414eeb319c643a035af5d76c0b6da2bb8330baa
Muhammad Beni Fajri <m.b.fajrii@gmail.com>
2024-02-19 12:44:20 +0700

    add : onBoarding screen only one time

---
+++ lib/data/repositories/authentication_repository.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerce/features/authentication/screens/login/login.dart';
+import 'package:e_commerce/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class AuthenticationRepository extends GetxController {
+  static AuthenticationRepository get instance => Get.find();
+
+  /// Variables
+  final deviceStorage = GetStorage();
+
+  /// Called from main.dart on app launch
+  @override
+  void onReady() {
+    FlutterNativeSplash.remove();
+    screenRedirect();
+  }
+
+  /// Function to show Relevant Screen
+  screenRedirect() async {
+    // Local Storage
+    deviceStorage.writeIfNull("isFirstTime", true);
+    deviceStorage.read("isFirstTime") != true ? Get.offAll(() => const LoginScreen()) : Get.offAll(const OnBoardingScreen());
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,26 @@
-import 'package:e_commerce/features/authentication/screens/onboarding/onboarding.dart';
+import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/theme/theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
+import 'data/repositories/authentication_repository.dart';
 import 'firebase_options.dart';
 
-Future<void> main() async  {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
-      // .then((FirebaseApp value) => Get.put(AuthenticationRepository()));
+Future<void> main() async {
+  /// Widgets Binding
+  final WidgetsBinding widgetsBinding =
+      WidgetsFlutterBinding.ensureInitialized();
+
+  /// GetX Local Storage
+  await GetStorage.init();
+
+  /// Await Splash until other items Load
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then((FirebaseApp value) => Get.put(AuthenticationRepository()));
 
   runApp(const App());
 }
@@ -25,8 +35,14 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      home: const OnBoardingScreen(),
+      home: const Scaffold(
+        backgroundColor: MyColors.primary,
+        body: Center(
+          child: CircularProgressIndicator(
+            color: MyColors.white,
+          ),
+        ),
+      ),
     );
   }
 }
-
